3b31bb00d81d0231047afb42f5eb01608527c0df
Anurag Gupta <anurag.gupta2@yum.com>
2024-12-25 08:07:40 +0530

    bugs

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 import { message } from "antd";
-// const baseURL = `https://api.starludo.club/api/v1/`;
+const baseURL = `https://api.starludo.club/api/v1/`;
 
-const baseURL = `http://157.173.218.246:5011/api/v1/`;
+// const baseURL = `http://157.173.218.246:5011/api/v1/`;
 // const baseURL = `http://localhost:5010/api/v1/`;
 let HELPERS = {
   getCookie: (name) => {
